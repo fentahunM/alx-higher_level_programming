d689dd3023220f9a8d5e90debe750d4073a73320
BeFun44 <fantahun360@gmail.com>
2023-03-28 15:40:33 -0700

    define a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,7 +9,7 @@ class Square():
     """Defines a square."""
 
     def __init__(self, size):
-        """Sets the necessary attributes for the Square object.
+        """Sets or initializes the necessary attributes for the Square object.
         Args:
             size (int): the size of one edge of the square.
         """
